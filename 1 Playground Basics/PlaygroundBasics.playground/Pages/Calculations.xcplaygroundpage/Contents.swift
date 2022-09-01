df14ca8bbb1e7d22ed0929f22b08e9030a7a7e56
Dgriscow <98046458+Dgriscow@users.noreply.github.com>
2022-09-01 12:43:47 -0500

    initial initializer

---
+++ 1 Playground Basics/PlaygroundBasics.playground/Pages/Calculations.xcplaygroundpage/Contents.swift
@@ -0,0 +1,34 @@
+//: ## Calculations
+//: You can use a playground like a calculator. Type in the math problem you want solved and the results sidebar shows the answer, or the _result_, of the calculation.
+//:
+//: You can use the plus `+` and minus `-` signs for addition and subtraction, just as you’d expect:
+34 + 56 + 230
+
+1000 - 300
+
+250-60
+880 + -900
+
+//: For multiplication, use the asterisk `*` :
+3 * 100
+
+5 * -10
+-5 * 2
+
+//: For division, use the slash `/` :
+24 / 8
+-26 / 6
+-6 / 4 + 3
+//: For grouping, use parenthesis `( )` :
+(300 + 500 + 400) / 3
+
+(288 + 399) * (300-3) / 2
+(-1 + 3) / 60 + (800 * 30 - 7 + -9)
+//: - experiment:\
+//: Practice using the playground like a calculator.
+//: Edit some of the existing expressions and add a few of your own.
+//: Notice that every time you make a change the results are updated in the sidebar.
+//:
+//: Next, learn how programmers can leave notes in code.
+//:
+//:[Previous](@previous)  |  page 3 of 7  |  [Next: Comments](@next)
